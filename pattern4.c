1ab943892a5dbb9a87d5f74fc2b0f2c4f7370c7b
TGX_HARRY <mittalhemang11@gmail.com>
2025-01-14 18:25:33 +0530

    Commit it

--- pattern4.c
+++ pattern4.c
@@ -5,7 +5,20 @@ int main() {
     int n;
     printf("Enter a number : ");
         scanf("%d", &n);
+
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            if (i==j || i+j == n+1) {
+                printf("*");
+            }
+            else {
+                printf(" ");
+                }
+            }
+            printf("\n");
+        }
+        
+        return 0;
+    }
+
     
-    
-    return 0;
-}
