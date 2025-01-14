18d1377e7b0ba424d250c3875afaecd2f7a5082a
TGX_HARRY <mittalhemang11@gmail.com>
2025-01-14 20:17:50 +0530

    commit_it_just

--- pattern4.c
+++ pattern4.c
@@ -6,11 +6,11 @@ int main() {
     printf("Enter a number : ");
         scanf("%d", &n);
 
-    for (int i=1; i<=n; i++) {
-        for (int j=1; j<=n; j++) {
-            if (i==j || i+j == n+1) {
-                printf("*");
-            }
+    for (int i=1; i<=n; i++) {              // outer loop for rows
+        for (int j=1; j<=n; j++) {          // inner loop for columns
+            if (i==j || i+j == n+1) {       // after analyzing pattern we needmain diagonal
+                printf("*");                // and sum of row and column number to be greater
+            }                               // greater than size of pattern
             else {
                 printf(" ");
                 }
