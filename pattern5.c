c5e021f3f1d8349f611c554a44ed9bc195330172
TGX-HARRY <mittalhemang11@gmail.com>
2025-01-16 16:46:03 +0530

    pattern5_hard

---
+++ pattern5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number : ");
+        scanf("%d", &n);
+
+    for (int i=1; i<=n; i++) {
+        for (int j=n; j>=i; j--) {
+            printf(" ");
+        }
+        for (int k=i; k>=2; k--) {
+            printf("%d", k);
+        }
+        for (int l=1; l<=i; l++) {
+            printf("%d", l);
+        }
+        printf("\n");
+    }
+    return 0;
+}
